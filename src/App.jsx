347085c7b2012bc5b73585bc6fa1f8aa3b819f81
Seoyeon Kim <kseoy0607@gmail.com>
2024-08-18 23:59:28 +0900

    240818 내용 변경

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,7 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
           </ul>
         </section>
       </main>
