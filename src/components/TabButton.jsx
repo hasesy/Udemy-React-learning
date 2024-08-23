9eecba8e30ca514faa84bb96eb9080d2c365f4ac
Seoyeon Kim <kseoy0607@gmail.com>
2024-08-23 13:16:05 +0900

    240823 2. 이벤트 처리 - onClick

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,14 @@
 export default function TabButton({ children }) {
+  function handleClick() {
+    console.log('Hello World!');
+  }
+
   return (
     <li>
       {/* 컴포넌트에서 children 이라는 속성을 전달해주는 게 아님.
         컴포넌트 텍스트 사이에 내용을 의미함 */}
-      <button>{children}</button>
+      {/* handleClick()으로 실행하면 안됨. 이름을 사용해야 함 */}
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
