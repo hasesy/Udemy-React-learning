83806042066fbea402418755e397d2cbd32444e8
Seoyeon Kim <kseoy0607@gmail.com>
2024-08-23 16:31:34 +0900

    240823 4. useState / 상태관리 & 훅 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
   function handleSelect(selectedButton) {
     // selectedButton = 'Components', 'JSX', 'Props', 'State'
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
 
   return (
@@ -35,7 +40,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {selectedTopic}
         </section>
       </main>
     </div>
