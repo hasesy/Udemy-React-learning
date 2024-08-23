aa1ef954e1008d0d621f1703f25aec52fe974339
Seoyeon Kim <kseoy0607@gmail.com>
2024-08-23 15:13:37 +0900

    240823 3. 이벤트 함수에 커스텀 인자 전달(익명함수 사용)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton = 'Components', 'JSX', 'Props', 'State'
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -21,11 +26,16 @@ function App() {
           <h2>Examples</h2>
           {/* menu = 버튼 목록 만들 때 사용하는 HTML 코드, 탭 만들 때 사용 */}
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            {/* 이 코드가 분석될 때 화살표 함수만 정의되기 때문에
+            화살표 함수 안에 코드는 아직 실행 X */}
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
