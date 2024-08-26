1e84541c65493963846275ab577607bc3b990237
Seoyeon Kim <kseoy0607@gmail.com>
2024-08-26 11:51:21 +0900

    240826 1. JSX를 사용하지 않는 방법

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./App.jsx";
-import "./index.css";
+import App from './App.jsx';
+import './index.css';
 
-const entryPoint = document.getElementById("root");
+const entryPoint = document.getElementById('root');
+// JSX 코드
 ReactDOM.createRoot(entryPoint).render(<App />);
+
+// JSX 코드의 유효한 대안
+// ReactDOM.createRoot(entryPoint).render(React.createElement(App));
