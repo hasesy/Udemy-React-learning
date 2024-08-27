f8b7f61d257e6962485972a8d3295c4859e76763
Seoyeon Kim <kseoy0607@gmail.com>
2024-08-27 21:37:37 +0900

    240827 2. 컴포넌트를  분리하여 App.jsx 파일 간단하게 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,16 @@
 import { useState } from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
-import { EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    // selectedButton = 'Components', 'JSX', 'Props', 'State'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
   return (
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          {/* menu = 버튼 목록 만들 때 사용하는 HTML 코드, 탭 만들 때 사용 */}
-          <menu>
-            {/* 이 코드가 분석될 때 화살표 함수만 정의되기 때문에
-            화살표 함수 안에 코드는 아직 실행 X */}
-            <TabButton
-              isSelected={selectedTopic === 'components'}
-              onSelect={() => handleSelect('components')}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'jsx'}
-              onSelect={() => handleSelect('jsx')}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'props'}
-              onSelect={() => handleSelect('props')}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'state'}
-              onSelect={() => handleSelect('state')}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   );
---
+++ src/components/Examples.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+import TabButton from './TabButton.jsx';
+import { EXAMPLES } from '../data.js';
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton = 'Components', 'JSX', 'Props', 'State'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      {/* menu = 버튼 목록 만들 때 사용하는 HTML 코드, 탭 만들 때 사용 */}
+      <menu>
+        {/* 이 코드가 분석될 때 화살표 함수만 정의되기 때문에
+            화살표 함수 안에 코드는 아직 실행 X */}
+        <TabButton
+          isSelected={selectedTopic === 'components'}
+          onSelect={() => handleSelect('components')}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === 'jsx'}
+          onSelect={() => handleSelect('jsx')}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === 'props'}
+          onSelect={() => handleSelect('props')}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === 'state'}
+          onSelect={() => handleSelect('state')}
+        >
+          State
+        </TabButton>
+      </menu>
+      {tabContent}
+    </section>
+  );
+}
