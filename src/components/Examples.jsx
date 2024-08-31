13666cf1d548041cb0d8a1d70222bffff9105c2f
Seoyeon Kim <kseoy0607@gmail.com>
2024-08-31 23:39:35 +0900

    240831 JSX 슬롯 활용법, 속성으로 jsx 코드 보내기

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import TabButton from './TabButton.jsx';
 import Section from './Section.jsx';
 import { EXAMPLES } from '../data.js';
+import Tabs from './Tabs.jsx';
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -29,36 +30,38 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      {/* menu = 버튼 목록 만들 때 사용하는 HTML 코드, 탭 만들 때 사용 */}
-      <menu>
-        {/* 이 코드가 분석될 때 화살표 함수만 정의되기 때문에
-            화살표 함수 안에 코드는 아직 실행 X */}
-        <TabButton
-          isSelected={selectedTopic === 'components'}
-          onClick={() => handleSelect('components')}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'jsx'}
-          onClick={() => handleSelect('jsx')}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'props'}
-          onClick={() => handleSelect('props')}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'state'}
-          onClick={() => handleSelect('state')}
-        >
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onClick={() => handleSelect('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onClick={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onClick={() => handleSelect('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onClick={() => handleSelect('state')}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
     </Section>
   );
 }
